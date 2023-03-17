e2ea20670f40dcfc74d09b666cc2739ef7716b84
Victoria Yakovleva <vikyss@mail.ru>
2023-03-17 21:59:51 +0100

    feat: delete unused files

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,4 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
 import {initMenu} from './modules/menu';
 import {initMap} from './modules/map';
@@ -19,7 +18,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
     initMenu();
     initMap({
       id: 'map',
